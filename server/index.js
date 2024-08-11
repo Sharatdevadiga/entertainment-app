f2d45a8e21899d7738c022d28669d9dd3eeba158
sharath <sharathdevadiga210@gmail.com>
2024-08-11 21:38:03 +0530

    changed CORS policy

--- server/index.js
+++ server/index.js
@@ -29,14 +29,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://entertainment-app-sandy.vercel.app/",
-];
-
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     credentials: true,
   })
 );
