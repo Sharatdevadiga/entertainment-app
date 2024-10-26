de9ea391da0d27d3a57e8194c7adb8c45fd0f5bc
sharath <sharathdevadiga210@gmail.com>
2024-10-26 15:52:01 +0530

    deployed server again

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,7 +2,7 @@
 // export const BASE_URL = "https://entertainmentapp.up.railway.app/";
 // export const BASE_URL = "https://entertainment-app-9bsw.onrender.com/";
 
-export const BASE_URL = "https://entertainment-app-sharath.onrender.com";
+export const BASE_URL = "https://entertainment-app-sharath.onrender.com/";
 
 export const RETRY = 3;
 export const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500/";
